1155b6d15ad7515c787f7a69b95751631d5c2597
poorvaditya18 <aditya.behre@gmail.com>
2024-08-18 01:59:37 +0530

    added Payment class in factory

--- LLD/Design_Patterns/factory.py
+++ LLD/Design_Patterns/factory.py
@@ -1,21 +1,33 @@
 """
 Factory method - lies under creational design patterns. 
                - Instead of the client code directly creating objects, it delegates the responsibility to a Factory Method.
+               - The Factory Design Pattern is a creational design pattern used to create objects without specifying the exact class of the object that will be created.
+               - Benefits : 
+                   - Encapsulation: The client code is decoupled from the specific classes needed to instantiate the logger. The factory handles this.
+                   - You can add new types of Payment without modifying the client code.
+                   - This pattern is useful in larger systems where object creation logic becomes more complex.
 """
-class Stripe:
+from abc import abstractmethod
+
+class Payment:
+    @abstractmethod
+    def make_payment(self,amount):
+        pass
+
+class Stripe(Payment):
 
     def __init__(self):
         self.name = 'razorpay'
 
-    def make_payment(self, amount):
+    def make_payment(self, amount : float ):
         print(f"{self.name} initiating payment {amount} ")
 
-class Razorpay:
+class Razorpay(Payment):
 
     def __init__(self):
         self.name = 'razorpay'
 
-    def make_payment(self, amount):
+    def make_payment(self, amount : float):
         print(f"{self.name} initiating payment {amount} ")
 
 class PaymentFactoryMethod:
